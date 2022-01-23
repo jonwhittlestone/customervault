fea1daf1d12ea855821217773bd1a626f7c35b93
Jon Whittlestone <dev@howapped.com>
2022-01-23 08:10:42 +0000

    Updates

---
+++ precache-manifest.df016f6986e6ac56197df3acdaa3af7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "193fca5c6c97d8bb91f4d34abdca8e5e",
+    "url": "/customervault/index.html"
+  },
+  {
+    "revision": "d28da2ea20293e75ec8d",
+    "url": "/customervault/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5caeaae02598237feb23",
+    "url": "/customervault/static/js/2.ea490775.chunk.js"
+  },
+  {
+    "revision": "f589bf591c94edd204e3",
+    "url": "/customervault/static/js/3.d439ba6f.chunk.js"
+  },
+  {
+    "revision": "d28da2ea20293e75ec8d",
+    "url": "/customervault/static/js/main.18727413.chunk.js"
+  },
+  {
+    "revision": "1dbb2b43eefca920af1d",
+    "url": "/customervault/static/js/runtime~main.8c247aa5.js"
+  },
+  {
+    "revision": "fd7efd2c2205ed83eba630753d44deb0",
+    "url": "/customervault/static/media/logo.fd7efd2c.svg"
+  }
+]);
